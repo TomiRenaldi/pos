0c092372e43999eb3300823aa7d387f49ee098c2
TomiRenaldi <tomirenaldi2@gmail.com>
2025-04-20 21:41:51 +0700

    :tada: Mari bercerita

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="font-bold text-blue-800">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
